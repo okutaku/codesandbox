297efd703a4b19037d139061ed55c91f48c1da97
okutaku <72905878+okutaku@users.noreply.github.com>
2020-12-30 08:47:06 +0900

    Appのクリックイベント・クラスの追加

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
-import "./styles.css";
 
-export default function App() {
+const App = () => {
+  const onClick = () => alert();
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
+  // スタイルを指定する場合は２種類あり、{}で中身を書くか、定数宣言してから中身を書いていくか
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <h1 style={{ color: "red" }}>こんにちは</h1>
+      <p style={contentStyle}>こんばんわ</p>
+      <button onClick={onClick}> のたん</button>
+    </>
   );
-}
+};
+
+export default App;
